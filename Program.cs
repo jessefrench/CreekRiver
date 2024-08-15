631ee37c2f47a5d61f7192a33053f9f1d37fa519
Jesse <frejes32@gmail.com>
2024-08-14 19:52:56 -0500

    Added POST endpoint

--- Program.cs
+++ Program.cs
@@ -45,4 +45,12 @@ app.MapGet("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
     return db.Campsites.Include(c => c.CampsiteType).Single(c => c.Id == id);
 });
 
+// Create campsite
+app.MapPost("/api/campsites", (CreekRiverDbContext db, Campsite campsite) =>
+{
+    db.Campsites.Add(campsite);
+    db.SaveChanges();
+    return Results.Created($"/api/campsites/{campsite.Id}", campsite);
+});
+
 app.Run();
